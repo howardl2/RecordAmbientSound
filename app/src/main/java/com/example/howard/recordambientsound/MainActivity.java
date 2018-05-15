17575f15371bd4b09cdedb25218954cdd36d7de4
Howard Liu <howardl2@uci.edu>
2018-05-15 01:52:16 -0700

    Initial commit

---
+++ app/src/main/java/com/example/howard/recordambientsound/MainActivity.java
@@ -0,0 +1,136 @@
+package com.example.howard.recordambientsound;
+
+import android.Manifest;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.LocalBroadcastManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import org.tensorflow.contrib.android.TensorFlowInferenceInterface;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    private final String TAG = "MainActivityTag";
+    private static final String PERMISSIONTAG = "permissions";
+
+    private Button startRecordingButton;
+    private TextView currTime;
+    private TextView prevTime;
+    private BroadcastReceiver receiver;
+    private static final int PERMISSIONS_REQUEST_RECORD_AUDIO = 1;
+    private static final int PERMISSIONS_REQUEST_WRITE_EXTERNAL_STORAGE = 2;
+    private static final int PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE = 3;
+    private static final int PERMISSIONS_ALL = 10;
+
+    private String prevTimeText;
+
+    private TensorFlowInferenceInterface inferenceInterface;
+    private static final String MODEL_FILE = "file:///android_asset/optimized_ambient_nn.pb";
+    private static final String INPUT_NODE = "first";
+    private static final String OUTPUT_NODE = "output";
+    private static final int[] INPUT_SIZE = {1,3};
+
+    static {
+        System.loadLibrary("tensorflow_inference");
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        inferenceInterface = new TensorFlowInferenceInterface(getAssets(), MODEL_FILE);
+
+
+        String[] PERMISSIONS = {Manifest.permission.RECORD_AUDIO, Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.READ_EXTERNAL_STORAGE};
+        if (!checkPermissions(this, PERMISSIONS)) {
+            ActivityCompat.requestPermissions(this, PERMISSIONS, PERMISSIONS_ALL);
+        }
+
+        startRecordingButton = (Button) findViewById(R.id.startRecording);
+        startRecordingButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                initRecordingAudio();
+            }
+        });
+        currTime = (TextView) findViewById(R.id.timeText);
+        prevTime = (TextView) findViewById(R.id.prevTimeText);
+
+        receiver = new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                prevTime.setText(currTime.getText());
+                String s = intent.getStringExtra(RecordingService.TIME_MESSAGE);
+                Log.d(TAG, s);
+                currTime.setText(s);
+
+
+            }
+        };
+
+    }
+
+    private boolean checkPermissions(Context context, String... permissions) {
+        if (android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && context != null && permissions != null) {
+            for (String permission : permissions) {
+                if (ActivityCompat.checkSelfPermission(context, permission) != PackageManager.PERMISSION_GRANTED) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case PERMISSIONS_REQUEST_RECORD_AUDIO: {
+                Log.d(PERMISSIONTAG,"RESULT PERMISSION RECORD AUDIO");
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    Log.d(PERMISSIONTAG,"PERMISSION WAS GRANTED");
+
+                }
+            }
+            case PERMISSIONS_REQUEST_WRITE_EXTERNAL_STORAGE: {
+                Log.d(PERMISSIONTAG,"RESULT PERMISSION WRITE EXTERNAL STORAGE");
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    Log.d(PERMISSIONTAG,"PERMISSION WAS GRANTED");
+
+                }
+            }
+            case PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE: {
+                Log.d(PERMISSIONTAG,"RESULT PERMISSION READ EXTERNAL STORAGE");
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    Log.d(PERMISSIONTAG,"PERMISSION WAS GRANTED");
+
+                }
+            }
+            case PERMISSIONS_ALL: {
+                Log.d(PERMISSIONTAG,"RESULT PERMISSION ALL");
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    Log.d(PERMISSIONTAG,"PERMISSION WAS GRANTED");
+
+                }
+            }
+        }
+    }
+
+    public int initRecordingAudio() {
+        Intent recordingIntent = new Intent(this, RecordingService.class);
+        startService(recordingIntent);
+        LocalBroadcastManager.getInstance(this).registerReceiver((receiver),
+                new IntentFilter(RecordingService.TIME_RESULT)
+        );
+        return 1;
+    }
+}
---
+++ app/src/main/java/com/example/howard/recordambientsound/RecordingService.java
@@ -0,0 +1,270 @@
+package com.example.howard.recordambientsound;
+
+import android.app.IntentService;
+import android.content.Intent;
+import android.media.AudioFormat;
+import android.media.AudioRecord;
+import android.media.MediaRecorder;
+import android.os.Environment;
+import android.support.v4.content.LocalBroadcastManager;
+import android.util.Log;
+import android.widget.TextView;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * Created by Howard on 11/13/2017.
+ */
+
+public class RecordingService extends IntentService {
+    private final String TAG = "recordingService";
+    private int currTime = 0;
+    private static final int RECORDER_BPP = 16;
+    private static final String AUDIO_RECORDER_FILE_EXT_WAV = ".wav";
+    private static final String AUDIO_RECORDER_FOLDER = "AmbientRecordings";
+    private static final String AUDIO_RECORDER_TEMP_FILE = "record_temp.raw";
+    private static final int RECORDER_SAMPLERATE = 44100;
+    private static final int RECORDER_CHANNELS = AudioFormat.CHANNEL_IN_STEREO;
+    private static final int RECORDER_AUDIO_ENCODING = AudioFormat.ENCODING_PCM_16BIT;
+    short[] audioData;
+    private AudioRecord recorder = null;
+    private int bufferSize = 0;
+    private Thread recordingThread = null;
+    private boolean isRecording = false;
+    int[] bufferData;
+    int bytesRecorded;
+    SimpleDateFormat sdf = new SimpleDateFormat("HH:mm:ss");
+    public String textTime;
+    private LocalBroadcastManager broadcaster;
+    static final public String TIME_MESSAGE = "TIME_MESSAGE";
+    static final public String TIME_RESULT = "TIME_RESULT";
+
+    public RecordingService() {
+        super("RecordingService");
+        bufferSize = AudioRecord.getMinBufferSize(RECORDER_SAMPLERATE,RECORDER_CHANNELS,RECORDER_AUDIO_ENCODING)*3;
+        audioData = new short [bufferSize]; //short array that pcm data is put into.
+        broadcaster = LocalBroadcastManager.getInstance(this);
+        Log.d(TAG, "Constructing recording service");
+    }
+
+    public void sendResult(String message) {
+        Intent intent = new Intent(TIME_RESULT);
+        if (message != null) {
+            intent.putExtra(TIME_MESSAGE, message);
+        }
+        broadcaster.sendBroadcast(intent);
+    }
+
+    @Override
+    protected void onHandleIntent(Intent intent) {
+        Log.d(TAG, "handling intent here");
+        while (true) {
+            long millis = System.currentTimeMillis();
+            int seconds = (int) (millis / 1000);
+            if (seconds - currTime >= 10) {
+                textTime = sdf.format(new Date());
+                Log.d(TAG, String.valueOf(textTime));
+                Log.d(TAG, String.valueOf(isRecording));
+                currTime = seconds;
+                if (isRecording) {
+                    stopRecording();
+                } else {
+                    sendResult(textTime);
+                    startRecording();
+                }
+            }
+        }
+    }
+
+    public void startRecording() {
+        isRecording = true;
+        recorder = new AudioRecord(MediaRecorder.AudioSource.MIC, RECORDER_SAMPLERATE, RECORDER_CHANNELS, RECORDER_AUDIO_ENCODING, bufferSize);
+        int i = recorder.getState();
+        if (i==1) {
+            recorder.startRecording();
+        }
+        recordingThread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                writeAudioDataToFile();
+            }
+        }, "AudioRecorder Thread");
+        recordingThread.start();
+    }
+
+    public void stopRecording() {
+        if (null != recorder) {
+            isRecording = false;
+            int i = recorder.getState();
+            if (i==1) {
+                recorder.stop();
+            }
+            recorder.release();
+            recorder = null;
+            recordingThread = null;
+        }
+        copyWaveFile(getTempFilename(),getFilename());
+        deleteTempFile();
+    }
+
+    public void writeAudioDataToFile() {
+        byte data[] = new byte[bufferSize];
+        String filename = getTempFilename();
+        FileOutputStream os = null;
+
+        try {
+            os = new FileOutputStream(filename);
+        } catch (FileNotFoundException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+        int read = 0;
+        if (null != os) {
+            while(isRecording) {
+                read = recorder.read(data, 0, bufferSize);
+                if (read > 0){
+                }
+
+                if (AudioRecord.ERROR_INVALID_OPERATION != read) {
+                    try {
+                        os.write(data);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+            try {
+                os.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private String getTempFilename() {
+        String filepath = Environment.getExternalStorageDirectory().getPath();
+        File file = new File(filepath,AUDIO_RECORDER_FOLDER);
+
+        if (!file.exists()) {
+            file.mkdirs();
+        }
+
+        File tempFile = new File(filepath,AUDIO_RECORDER_TEMP_FILE);
+
+        if (tempFile.exists())
+            tempFile.delete();
+
+        return (file.getAbsolutePath() + "/" + AUDIO_RECORDER_TEMP_FILE);
+    }
+
+
+    private void copyWaveFile(String inFilename, String outFilename) {
+        FileInputStream in = null;
+        FileOutputStream out = null;
+        long totalAudioLen = 0;
+        long totalDataLen = totalAudioLen + 36;
+        long longSampleRate = RECORDER_SAMPLERATE;
+        int channels = 2;
+        long byteRate = RECORDER_BPP * RECORDER_SAMPLERATE * channels/8;
+
+        byte[] data = new byte[bufferSize];
+        try {
+            in = new FileInputStream(inFilename);
+            out = new FileOutputStream(outFilename);
+            totalAudioLen = in.getChannel().size();
+            totalDataLen = totalAudioLen + 36;
+
+            Log.d(TAG, "File size: " + totalDataLen);
+
+            WriteWaveFileHeader(out, totalAudioLen, totalDataLen, longSampleRate, channels, byteRate);
+
+            while(in.read(data) != -1) {
+                out.write(data);
+            }
+
+            in.close();
+            out.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void WriteWaveFileHeader(FileOutputStream out, long totalAudioLen,
+                                     long totalDataLen, long longSampleRate,
+                                     int channels, long byteRate) throws IOException {
+        byte[] header = new byte[44];
+
+        header[0] = 'R';  // RIFF/WAVE header
+        header[1] = 'I';
+        header[2] = 'F';
+        header[3] = 'F';
+        header[4] = (byte) (totalDataLen & 0xff);
+        header[5] = (byte) ((totalDataLen >> 8) & 0xff);
+        header[6] = (byte) ((totalDataLen >> 16) & 0xff);
+        header[7] = (byte) ((totalDataLen >> 24) & 0xff);
+        header[8] = 'W';
+        header[9] = 'A';
+        header[10] = 'V';
+        header[11] = 'E';
+        header[12] = 'f';  // 'fmt ' chunk
+        header[13] = 'm';
+        header[14] = 't';
+        header[15] = ' ';
+        header[16] = 16;  // 4 bytes: size of 'fmt ' chunk
+        header[17] = 0;
+        header[18] = 0;
+        header[19] = 0;
+        header[20] = 1;  // format = 1
+        header[21] = 0;
+        header[22] = (byte) channels;
+        header[23] = 0;
+        header[24] = (byte) (longSampleRate & 0xff);
+        header[25] = (byte) ((longSampleRate >> 8) & 0xff);
+        header[26] = (byte) ((longSampleRate >> 16) & 0xff);
+        header[27] = (byte) ((longSampleRate >> 24) & 0xff);
+        header[28] = (byte) (byteRate & 0xff);
+        header[29] = (byte) ((byteRate >> 8) & 0xff);
+        header[30] = (byte) ((byteRate >> 16) & 0xff);
+        header[31] = (byte) ((byteRate >> 24) & 0xff);
+        header[32] = (byte) (2 * 16 / 8);  // block align
+        header[33] = 0;
+        header[34] = RECORDER_BPP;  // bits per sample
+        header[35] = 0;
+        header[36] = 'd';
+        header[37] = 'a';
+        header[38] = 't';
+        header[39] = 'a';
+        header[40] = (byte) (totalAudioLen & 0xff);
+        header[41] = (byte) ((totalAudioLen >> 8) & 0xff);
+        header[42] = (byte) ((totalAudioLen >> 16) & 0xff);
+        header[43] = (byte) ((totalAudioLen >> 24) & 0xff);
+
+        out.write(header, 0, 44);
+    }
+
+    private String getFilename() {
+        String filepath = Environment.getExternalStorageDirectory().getPath();
+        File file = new File(filepath,AUDIO_RECORDER_FOLDER);
+
+        if (!file.exists()) {
+            file.mkdirs();
+        }
+        String timeForFile = sdf.format(new Date());
+        return (file.getAbsolutePath() + "/" + timeForFile + AUDIO_RECORDER_FILE_EXT_WAV);
+    }
+
+    private void deleteTempFile() {
+        File file = new File(getTempFilename());
+        file.delete();
+    }
+
+}
